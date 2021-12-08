20fa253156474fc3e9cfb60651049f02c84fa1c7
Sasha <sashajsirota@gmail.com>
2021-12-08 14:04:45 -0500

    clean unused code

--- client/src/components/NetworkChart/NetworkChart.js
+++ client/src/components/NetworkChart/NetworkChart.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
-import { ForceGraph3D } from "react-force-graph";
 
 export default class NetworkChart extends Component {
   state = {
@@ -8,32 +6,15 @@ export default class NetworkChart extends Component {
     isModalOpen: false,
   };
 
-  // componentDidMount() {
-  //   axios
-  //     .get(`http://localhost:8080/game/${this.props.currentGame}`)
-  //     .then((r) => {
-  //       this.setState({ data: r.data });
-  //     });
-  // }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log(this.state.data)
+    console.log(this.state.data);
   }
 
   render() {
     if (Object.keys(this.state.data).length === 0) {
       return <div>Loading...</div>;
     }
-    return (
-
-        <ForceGraph3D
-          graphData={this.state.data}
-          nodeLabel={(node) => {
-            console.log(node)
-          }}
-          backgroundColor={"rgb(27, 40, 56)"}
-        />
-
-    );
+    return <div></div>;
   }
 }
--- client/src/pages/NetworkPage/NetworkPage.js
+++ client/src/pages/NetworkPage/NetworkPage.js
@@ -1,40 +1,22 @@
 import React, { Component } from "react";
-// import NetworkChart from "../../components/NetworkChart/NetworkChart";
 import Select from "react-select";
 import allGamesData from "../../data/allGamesData.json";
 import initData from "../../data/CounterStrikeData.json";
 import axios from "axios";
 import { ForceGraph3D } from "react-force-graph";
 
-
 export default class NetworkPage extends Component {
   state = {
     data: initData,
-    // currentGame: 20,
     allGames: allGamesData,
   };
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if(prevState.currentGame !== this.state.currentGame) {
-  //     this.setState({currentGame: this.state.currentGame})
-  //   }
-  // }
-  componentDidUpdate(prevProps, prevState) {
-    // console.log(this.state.data)
-  }
-
   handleChange = (selection) => {
-    // this.setState({ currentGame: selection.appid });
     axios.get(`http://localhost:8080/game/${selection.appid}`).then((r) => {
       this.setState({ data: r.data });
-      // console.log(r.data)
-      console.log(this.state.data)
+      console.log(this.state.data);
     });
   };
-
-  // getCurrentGame = () => {
-  //   return this.state.currentGame;
-  // };
 
   render() {
     return (
@@ -44,18 +26,19 @@ export default class NetworkPage extends Component {
           options={this.state.allGames}
           onChange={this.handleChange}
         />
-        {/* <NetworkChart
-          // currentGame={this.state.currentGame}
-          newData={this.state.data}
-        /> */}
         <ForceGraph3D
           graphData={this.state.data}
           nodeLabel={(node) => {
-            console.log(node);
+            return (
+              <div>
+                <p>{node.name}</p>
+                <br></br>
+                <p>Frequency: {node.radius}</p>
+              </div>
+            );
           }}
           backgroundColor={"rgb(27, 40, 56)"}
         />
-        ;
       </article>
     );
   }
